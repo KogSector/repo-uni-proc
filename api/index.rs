e57c1acc71e514912a1d13240cdec3f09004b2a3
Rishabh Das <rishabh.babi@gmail.com>
2026-07-16 02:37:24 +0530

    .

--- api/index.rs
+++ api/index.rs
@@ -28,7 +28,7 @@ async fn main() -> anyhow::Result<()> {
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::try_from_default_env()
-            .unwrap_or_else(|_| "unified_processor=debug,tower_http=debug".into()))
+            .unwrap_or_else(|_| "unified_processor_lib=debug,unified_processor=debug,tower_http=debug".into()))
         .with(
             tracing_subscriber::fmt::layer()
                 .with_writer(std::io::stdout)
